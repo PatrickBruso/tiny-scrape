48d8fd748f26c7129fad5c5091735c745635f031
Patrick <pkbruso@gmail.com>
2021-11-12 15:59:32 -0800

    Sample program to scrape Algo price from tinychart.org

--- price-scraper.py
+++ price-scraper.py
@@ -1,31 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
-import pandas as pd
-import json
-import time
 
-# get full html from tiny chart:
+# get full html from tinychart:
 tiny = requests.get('https://tinychart.org/')
 
 # let BeautifulSoup format the html so you can read it
 tiny_soup = BeautifulSoup(tiny.content, 'html.parser')
 
-# look for specifically the script we know our data is in:
-data = tiny_soup.find('script', id="__NEXT_DATA__", type="application/json")
+# Find Algo price on tinychart and save to variable
+algo_data = tiny_soup.find('span', "ant-tag").text
+algo_price = float(algo_data[algo_data.find('$') + 1:])
 
-# unsorted complete data
-coin_data = json.loads(data.contents[0])
-# print(json.dumps(coin_data, indent=4, sort_keys=True))
-AlgData = tiny_soup.find('span', "ant-tag").text
-AlgoP = float(AlgData[AlgData.find('$') + 1:])
-print(AlgoP)
-
-# basic coin data:
-listings = coin_data['props']['pageProps']['assets']
-counter = 0
-for coin in listings:
-    if coin['total_usd_reserves'] > 10000:
-        # print(json.dumps(coin, indent=4, sort_keys=True))
-        counter += 1
-print(counter)
-print(json.dumps(listings, indent=4, sort_keys=True))
+print(f'Current price of Algo: ${algo_price}')
